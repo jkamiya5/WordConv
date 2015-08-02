8e21774bff3087a89336e62775259e075d015696
jkamiya5 <jkamiya5@gmail.com>
2015-08-02 22:16:47 +0900

    コミット

---
+++ SQLite/Form/IkkatsuTorokuService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WordConvertTool;
+
+namespace SQLite.Form
+{
+    class IkkatsuTorokuService
+    {
+        private IkkatsuTorokuServiceInBo inBo = new IkkatsuTorokuServiceInBo();
+
+        internal void setInBo(IkkatsuTorokuServiceInBo InBo)
+        {
+            this.inBo = InBo;
+        }
+
+        internal IkkatsuTorokuServiceOutBo execute()
+        {
+            IkkatsuTorokuServiceOutBo outBo = new IkkatsuTorokuServiceOutBo();
+
+            List<HenshuWordBo> wordList = new List<HenshuWordBo>();
+
+            if (!String.IsNullOrEmpty(this.inBo.clipboardText))
+            {
+                string key = this.inBo.clipboardText;
+                string nl = Environment.NewLine;
+                String[] keys = key.Split(new string[] { nl }, StringSplitOptions.None);
+                bool isExistWordDic = false;
+                foreach (String ronriName in keys)
+                {
+                    isExistWordDic = false;
+
+                    using (var context = new MyContext())
+                    {
+                        IQueryable<HenshuWordBo> words = from a in context.WordDic
+                                                         join b in context.UserMst on a.USER_ID equals b.USER_ID
+                                                         select new HenshuWordBo
+                                                         {
+                                                             WORD_ID = a.WORD_ID,
+                                                             RONRI_NAME1 = a.RONRI_NAME1,
+                                                             BUTSURI_NAME = a.BUTSURI_NAME,
+                                                             USER_NAME = b.USER_NAME,
+                                                             CRE_DATE = a.CRE_DATE,
+                                                             VERSION = (int)a.VERSION
+                                                         };
+                        String condition = ronriName.Trim();
+                        HenshuWordBo[] dispWords = words.Where(x => x.RONRI_NAME1 == condition).ToArray();
+
+                        foreach (var word in dispWords)
+                        {
+                            HenshuWordBo w = new HenshuWordBo();
+                            w.WORD_ID = word.WORD_ID;
+                            w.RONRI_NAME1 = word.RONRI_NAME1;
+                            w.BUTSURI_NAME = word.BUTSURI_NAME;
+                            w.USER_NAME = word.USER_NAME;
+                            w.CRE_DATE = word.CRE_DATE;
+                            w.VERSION = (int)word.VERSION;
+                            wordList.Add(w);
+                            isExistWordDic = true;
+                        }
+                    }
+                    if (!isExistWordDic)
+                    {
+                        HenshuWordBo w = new HenshuWordBo();
+                        w.RONRI_NAME1 = ronriName;
+                        wordList.Add(w);
+                    }
+                }
+            }
+            outBo.henshuWordBoList = wordList;
+            return outBo;
+        }
+    }
+}
