91b08c88ea5f3da858e0ea936fc4c87086988d2d
jkamiya5 <jkamiya5@gmail.com>
2015-08-04 18:19:31 +0900

    コミット

---
+++ SQLite/Interface/ShinseiInitInBo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WordConvTool.Model;
+
+namespace SQLite.Form
+{
+    public class ShinseiInitServiceInBo
+    {
+        public string clipboardText { get; set; }
+        public ShoriMode shoriMode { get; set; }
+
+    }
+
+}
